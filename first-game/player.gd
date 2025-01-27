fca693f1ac2df49ca2197e3923dbc8bebd53d8cd
Heiko <heikopedersen@gmail.com>
2025-01-27 08:54:54 +0100

    collision

--- first-game/player.gd
+++ first-game/player.gd
@@ -1,3 +1,9 @@
 extends Area2D
 @export var speed = 400 # How fast the player will move (pixels/sec).
 var screen_size # Size of the game window.
+
+func _on_body_entered(body: Node2D) -> void:
+	hide()
+	hit.emit()
+	$CollisionShape2D.set_deferred("disabled", true)
+	
