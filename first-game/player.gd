6cd161dca50136e1c805034e34eecb7a99ab8498
Heiko <heikopedersen@gmail.com>
2025-01-27 08:09:51 +0100

    yuughgh

--- first-game/player.gd
+++ first-game/player.gd
@@ -1,11 +1,3 @@
 extends Area2D
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+@export var speed = 400 # How fast the player will move (pixels/sec).
+var screen_size # Size of the game window.
