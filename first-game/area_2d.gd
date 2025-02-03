5a59b0a3ca590727e66b4d8cfa96d4416925c017
Maggen12 <magnus.k.brandhoej@gmail.com>
2025-02-03 08:25:43 +0100

    en masse ting 2

--- first-game/area_2d.gd
+++ first-game/area_2d.gd
@@ -6,6 +6,7 @@ var screen_size # Size of the game window.
 
 func _ready():
 	screen_size = get_viewport_rect().size
+	hide()
 	
 
 func _process(delta):
@@ -36,12 +37,12 @@ func _process(delta):
 		$AnimatedSprite2D.flip_v = velocity.y > 0
 
 
-func _on_body_entered(body: Node2D) -> void:
+func _on_body_entered(_body: Node2D) -> void:
 	hide()
 	hit.emit()
 	$CollisionShape2D.set_deferred("disabled", true)
 
-func start(pos):
+func start(pos : Vector2):
 	position = pos
 	show()
 	$CollisionShape2D.disabled = false 
--- first-game/main.gd
+++ first-game/main.gd
@@ -3,30 +3,24 @@ extends Node
 @export var mob_scene: PackedScene
 var score
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	new_game()
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
 func game_over() -> void:
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 
 func new_game():
 	score=0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	get_tree().call_group("mobs", "queue_free")
 
 
 func _on_mob_timer_timeout() -> void:
 	var mob = mob_scene.instantiate()
 	var mob_spawn_location = $MobPath/MobSpawnLocation
+	mob_spawn_location.progress_ratio = randf()
 	var direction = mob_spawn_location.rotation + PI / 2
 	mob.position = mob_spawn_location.position
 	direction += randf_range(-PI / 4, PI / 4)
@@ -38,8 +32,12 @@ func _on_mob_timer_timeout() -> void:
 
 
 func _on_score_timer_timeout() -> void:
-	pass # Replace with function body.
+	score += 1
+	$HUD.update_score(score)
 
 
 func _on_start_timer_timeout() -> void:
-	pass # Replace with function body.
+	$MobTimer.start()
+	$ScoreTimer.start()
+	
+	
